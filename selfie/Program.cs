4ae626c14d78776e2119c4c14717bdb3bdb58ddc
fhunter <fhunter@wsu.edu>
2014-04-25 12:50:56 -0700

    first commit

---
+++ selfie/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace selfie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("GetEnvironmentVariables: ");
+            IDictionary environmentVariables = Environment.GetEnvironmentVariables();
+            foreach (DictionaryEntry de in environmentVariables)
+            {
+                Console.WriteLine("  {0} = {1}", de.Key, de.Value);
+            }
+        }
+    }
+}
+
+
