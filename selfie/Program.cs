34dfd30eb3d26dcc765b2aecf78b55cd0f38fb0a
SmurfSlayer <hunter.fred@gmail.com>
2015-03-03 16:09:49 -0800

    Update Program.cs
    added something from GitHub

--- selfie/Program.cs
+++ selfie/Program.cs
@@ -9,7 +9,7 @@ namespace selfie
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Taking a selfie...");
+            Console.WriteLine("Taking a selfie2...");
             string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
             Console.WriteLine(userName);
             Console.WriteLine("GetEnvironmentVariables: ");
